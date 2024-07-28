167d5d65508bd5944cfc95bde0ab59b5a48ca758
Mark Mwima <markmwimar@gmail.com>
2024-07-28 23:29:08 +0300

    casting and type conversion

---
+++ Vipi.java
@@ -0,0 +1,29 @@
+class Vipi {
+    
+    public static void main(String a[]){
+    
+        char n = 'k';
+        int num = 0x7E;
+
+        byte b = 127;
+
+        int size = 25;
+
+
+        //Explicit assigning also known as casting
+        // b = (byte) size;
+
+        //Implicit assigning aslo known as conversion
+         size=b;
+
+         float f = 5.5f;
+         int num1 = (int) f;
+        System.out.println(num1);
+
+        //Type conversion
+        byte x = 7;
+        byte y = 3;
+        int result = x * y;
+        System.out.println(result);
+    }
+}
