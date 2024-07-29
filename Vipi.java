c0d525af21a2f31a794f2ed6350dce87523b5304
Mark Mwima <markmwimar@gmail.com>
2024-07-29 16:09:54 +0300

    else if

--- Vipi.java
+++ Vipi.java
@@ -39,5 +39,21 @@ class Vipi {
 
         System.out.println(result);
 
+        int d = 7;
+        if(d==1)
+            System.out.println("Monday");
+        else if(d==2)
+            System.out.println("Tuesday");
+        else if(d==3)
+            System.out.println("Wednesday");
+        else if(d==4)
+            System.out.println("Thursday");
+        else if(d==5)
+            System.out.println("Friday");
+        else if(d==6)
+            System.out.println("Saturday");
+        else
+        System.out.println("Sunday");
+
     }
 }
