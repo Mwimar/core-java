002d262f89ba9228e0792674e84fc2658ae4f3cc
Mark Mwima <markmwimar@gmail.com>
2024-07-29 16:20:30 +0300

    switch case

--- Vipi.java
+++ Vipi.java
@@ -39,21 +39,34 @@ class Vipi {
 
         System.out.println(result);
 
-        int d = 7;
-        if(d==1)
-            System.out.println("Monday");
-        else if(d==2)
-            System.out.println("Tuesday");
-        else if(d==3)
-            System.out.println("Wednesday");
-        else if(d==4)
-            System.out.println("Thursday");
-        else if(d==5)
-            System.out.println("Friday");
-        else if(d==6)
-            System.out.println("Saturday");
-        else
-        System.out.println("Sunday");
+        int d = 9;
+        switch (d){
+
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter a number between 0 and 8");
+            break;
+        }
 
     }
 }
