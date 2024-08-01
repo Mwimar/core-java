bfede0df1c0af48f03ff6387b698ef55531adc6b
Mark Mwima <markmwimar@gmail.com>
2024-08-01 13:09:08 +0300

    void method

--- Vipi.java
+++ Vipi.java
@@ -1,28 +1,15 @@
-class Calculator {
-    
+class Computer {
 
-    public int add(int n1, int n2) {
-        
-        int ans = n1 + n2;
+    public void playMusic() {
+        System.out.println("Playing music");
 
-        return ans;
-
-    }}
-
+    }
+  }
 
 
 class Vipi {
 
     public static void main(String a[]) {
-        int num1 = 7;
-        int num2 = 6;
-        
-
-        Calculator calc = new Calculator();
-        int result = calc.add(num1,num2);
-        System.out.println(result);
-
-
 
     }
     }
