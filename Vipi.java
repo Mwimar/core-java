8be989eab517068121049ff67557f960070ef0e7
Mark Mwima <markmwimar@gmail.com>
2024-08-01 22:11:59 +0300

    printing jagged array

--- Vipi.java
+++ Vipi.java
@@ -30,19 +30,19 @@ class Vipi {
         nums[2] = new int[2];
 
         
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i <nums.length; i++) {
+            for (int j = 0; j <nums[i].length; j++) {
                 nums[i][j] = (int) (Math.random()*10);
             }
             
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(nums[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // for (int i = 0; i < 3; i++) {
+        //     for (int j = 0; j < 4; j++) {
+        //         System.out.print(nums[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
 
         for (int n[] : nums) {
             for (int m : n) {
