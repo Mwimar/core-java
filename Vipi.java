77784009a3ef8b1d166f09b4978bb05c7ad1fdf6
Mark Mwima <markmwimar@gmail.com>
2024-08-01 22:15:23 +0300

    3D array

--- Vipi.java
+++ Vipi.java
@@ -24,18 +24,20 @@ class Vipi {
     public static void main(String a[]) {
 
         // int nums[][] = new int[3][4];
-        int nums[][] = new int[3][];// jagged array
-        nums[0] = new int[3];
-        nums[1] = new int[4];
-        nums[2] = new int[2];
+        // int nums[][] = new int[3][];// jagged array
+        // nums[0] = new int[3];
+        // nums[1] = new int[4];
+        // nums[2] = new int[2];
+
+        int nums[][][] = new int[3][4][5];//3D Array;
 
         
-        for (int i = 0; i <nums.length; i++) {
-            for (int j = 0; j <nums[i].length; j++) {
-                nums[i][j] = (int) (Math.random()*10);
-            }
+        // for (int i = 0; i <nums.length; i++) {
+        //     for (int j = 0; j <nums[i].length; j++) {
+        //         nums[i][j] = (int) (Math.random()*10);
+        //     }
             
-        }
+        // }
 
         // for (int i = 0; i < 3; i++) {
         //     for (int j = 0; j < 4; j++) {
@@ -44,12 +46,12 @@ class Vipi {
         //     System.out.println();
         // }
 
-        for (int n[] : nums) {
-            for (int m : n) {
-                System.out.print(m + " ");
-            }
-            System.out.println();
-        }
+        // for (int n[] : nums) {
+        //     for (int m : n) {
+        //         System.out.print(m + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
     }
 
