7d9853b5aa6207c412a6f161998dc3f14bb95649
Mark Mwima <markmwimar@gmail.com>
2024-08-01 20:43:20 +0300

    multidimensional array

--- Vipi.java
+++ Vipi.java
@@ -23,15 +23,13 @@ class Vipi {
 
     public static void main(String a[]) {
 
-        int nums[] = new int[4];
-        nums[0] = 5;
-        nums[1] = 2;
-        nums[2] = 9;
-        nums[3] = 4;
+        int nums[][] = new int[3][4];
 
-        for ( int i = 0; i <= 3; i++) {
-            
-            System.out.println(nums[i]);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
         }
     }
     }
