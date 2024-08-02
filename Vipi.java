f86613447300b914a92807519ce42c704dde2989
Mark Mwima <markmwimar@gmail.com>
2024-08-02 14:59:56 +0300

    static variables

--- Vipi.java
+++ Vipi.java
@@ -1,7 +1,7 @@
 class Mobile {
     String brand;
     int price;
-    String name;
+    static String name;
 
     public void show() {
         System.out.println(brand+ ":" + price+ ":" + name);
@@ -18,12 +18,12 @@ class Vipi {
         Mobile mobile1 = new Mobile();
         mobile1.brand = "Tecno";
         mobile1.price = 50000;
-        mobile1.name = "Spark";
+        Mobile.name = "Spark";
 
         Mobile mobile2 = new Mobile();
         mobile2.brand = "Huawei";
         mobile2.price = 7000;
-        mobile2.name = "Matte";
+        Mobile.name = "Matte";
 
         mobile1.show();
         mobile2.show();
