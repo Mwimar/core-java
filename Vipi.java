d5b95955282812e95cf74558776a5c65e6352f0c
Mark Mwima <markmwimar@gmail.com>
2024-08-04 21:15:50 +0300

    using inherited class methods

--- Vipi.java
+++ Vipi.java
@@ -6,7 +6,9 @@ class Vipi {
        
         int r1 = obj.add(4, 8);
         int r2 = obj.sub(7, 3);
-        System.out.println("r1:" + r1 + " " + "r2:" + r2);
+        int r3 = obj.mul(4, 4);
+        int r4 = obj.div(9, 3);
+        System.out.println("r1:" + r1 + " " + "r2:" + r2 + " " + "r3:" + r3 + " " + "r4:" + r4);
         
 
          }
