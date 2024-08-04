ca411253d5bf3912d0c2730393f9bab1091bb6c5
Mark Mwima <markmwimar@gmail.com>
2024-08-04 20:43:48 +0300

    calc class

--- Vipi.java
+++ Vipi.java
@@ -1,47 +1,15 @@
-
-
-class Human {
-    private String name;
-    private int age;
-
-   
-
-    public Human(String name, int age) {
-        this.name = name;
-        this.age = age;
-        
-    }
+class Calc{
     
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-   
-   
-
 }
+
+
 
 class Vipi {
 
-    public static void main(String a[]) throws ClassNotFoundException {
+    public static void main(String a[]) {
 
        
 
-        
-        Human obj1 = new Human("Kevin", 50);
-        // obj.setAge(26);
-        // obj.setName("Mwima");
-        
-        System.out.println( obj1.getName() + ":" + obj1.getAge());
-
-    }
+         }
 
 }
