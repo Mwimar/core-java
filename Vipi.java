86a836daa2dc0478f2d24b519c433092908d0bc3
Mark Mwima <markmwimar@gmail.com>
2024-08-04 20:59:33 +0300

    calc class

--- Vipi.java
+++ Vipi.java
@@ -1,24 +1,10 @@
-class Calc {
-    
-    public int add(int n1, int n2) {
-        return n1 + n2;
-
-    }
-
-     public int sub(int n1, int n2) {
-        return n1 - n2;
-
-    }
-
-}
-
-
-
 class Vipi {
 
     public static void main(String a[]) {
 
+        Calc obj = new Calc();
        
+        
 
          }
 
