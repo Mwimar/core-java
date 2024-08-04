f924bcc40d4b66bf17a5b8e26f626b257ca27d5c
Mark Mwima <markmwimar@gmail.com>
2024-08-04 21:35:21 +0300

    multilevel inheritance

--- VeryAdvCalc.java
+++ VeryAdvCalc.java
@@ -1,4 +1,4 @@
-public class VeryAdvCalc {
+public class VeryAdvCalc extends AdvCalc{
     
     public double power(int n1, int n2){
         return Math.pow(n1, n1);
--- Vipi.java
+++ Vipi.java
@@ -2,13 +2,14 @@ class Vipi {
 
     public static void main(String a[]) {
 
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
        
         int r1 = obj.add(4, 8);
         int r2 = obj.sub(7, 3);
         int r3 = obj.mul(4, 4);
         int r4 = obj.div(9, 3);
-        System.out.println("r1:" + r1 + " " + "r2:" + r2 + " " + "r3:" + r3 + " " + "r4:" + r4);
+        double r5 = obj.power(5, 3);
+        System.out.println("r1:" + r1 + " " + "r2:" + r2 + " " + "r3:" + r3 + " " + "r4:" + r4+ " " + "r5:" + r5);
         
 
          }
