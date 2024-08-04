a9cab7b87c3f7b8c1afc597a2f0cc5cf79a9ac46
Mark Mwima <markmwimar@gmail.com>
2024-08-04 21:10:13 +0300

    creating inheritance

--- AdvCalc.java
+++ AdvCalc.java
@@ -1,4 +1,4 @@
-public class AdvCalc {
+public class AdvCalc extends Calc {
 
     public int mul(int n1, int n2) {
         return n1 * n2;
--- Vipi.java
+++ Vipi.java
@@ -2,8 +2,11 @@ class Vipi {
 
     public static void main(String a[]) {
 
-        Calc obj = new Calc();
+        AdvCalc obj = new AdvCalc();
        
+        int r1 = obj.add(4, 8);
+        int r2 = obj.sub(7, 3);
+        System.out.println("r1:" + r1 + " " + "r2:" + r2);
         
 
          }
