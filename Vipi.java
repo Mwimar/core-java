c4ce53f7eaa45ad8aab511f27f0e7941d29b0a20
Mark Mwima <markmwimar@gmail.com>
2024-08-05 21:23:29 +0300

    Integer class

--- Vipi.java
+++ Vipi.java
@@ -16,10 +16,14 @@ class B extends A {
 class Vipi {
 
   public static void main(String a[]) {
-    A obj = (A) new B();
-    obj.show1();
+    // A obj = (A) new B();
+    // obj.show1();
 
-    B obj1 = (B) obj;
-    obj1.show2();
+    // B obj1 = (B) obj;
+    // obj1.show2();
+
+    int num = 5;
+    Integer numb = new Integer(4);
+    System.out.println(numb);
   }
 }
