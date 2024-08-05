a12836b0134fa24cbb73b1f2ddc5890fe81bf01c
Mark Mwima <markmwimar@gmail.com>
2024-08-05 20:50:00 +0300

    updating equals method

--- Vipi.java
+++ Vipi.java
@@ -9,12 +9,9 @@ class Laptop {
   }
 
   public boolean equals(Laptop that) {
-    if (this.model.equals(that.model) && this.price == that.price) {
-      return true;
-    }
-    else {
-      return false;
-    }
+
+    return this.model.equals(that.model) && this.price == that.price;
+    
   }
  }
 
