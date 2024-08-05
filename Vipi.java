b45080014a16e63a5c53eb020048472b78a63302
Mark Mwima <markmwimar@gmail.com>
2024-08-05 13:29:52 +0300

    dynamic method dispatch

--- Vipi.java
+++ Vipi.java
@@ -1,20 +1,35 @@
 
 class A {
+  public void show() {
+    System.out.println("In A show");
+  }
 }
 
 
 class B extends A {
-}
+  public void show() {
+    System.out.println("In B show");
 
+  }
+
+}
 
 class C extends A {
+  public void show() {
+    System.out.println("In c show");
+  }
 }
-
 
 
 class Vipi {
 
   public static void main(String a[]) {
 
+    A obj = new A();
+    obj = new B();
+    obj.show();
+
+    obj = new C();
+    obj.show();
   }
 }
