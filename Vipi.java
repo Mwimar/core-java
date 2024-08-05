7966c8a0b81139f682df94162a7641168d759880
Mark Mwima <markmwimar@gmail.com>
2024-08-05 13:45:18 +0300

    final class

--- Vipi.java
+++ Vipi.java
@@ -1,14 +1,25 @@
 
+final class Calc {
+  public void show() {
+    System.out.println("in Calc");
 
+  }
 
+  public void add(int a, int b) {
+    System.out.println(a + b);
+  }
+}
+
+// class AdvCalc extends Calc {
+  
+// }
 
 class Vipi {
 
   public static void main(String a[]) {
-
-    final int num = 7;
-    
-    System.out.println(num);
+    Calc obj = new Calc();
+    obj.show();
+    obj.add(7, 9);
    
   }
 }
