e5de7563a0429c6cdecbf320124202b23ef9d31b
Mark Mwima <markmwimar@gmail.com>
2024-08-05 21:12:55 +0300

    upcasting

--- Vipi.java
+++ Vipi.java
@@ -1,13 +1,22 @@
 
+class A {
 
+  public void show1() {
+    System.out.println("in A show");
+  }
+}
+
+class B extends A {
+  public void show2() {
+    System.out.println("in B show");
+  }
+}
 
 
 class Vipi {
 
   public static void main(String a[]) {
-    double num1 = 3.4;
-    int num2 = (int) num1;
-
-    System.out.println(num2);
+    A obj = (A) new B();
+    obj.show1();
   }
 }
