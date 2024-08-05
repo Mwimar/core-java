05112d85907d5242e559354388ec6faf2b4bc4dd
Mark Mwima <markmwimar@gmail.com>
2024-08-05 10:52:19 +0300

    super method

--- Vipi.java
+++ Vipi.java
@@ -1,19 +1,35 @@
+class A {
+    public A() {
+        super();
+        System.out.println("in A");
+    }
 
+    public A(int n) {
+        super();
+        System.out.println("in A int");
+    }
+
+}
+
+class B extends A {
+
+    public B() {
+        super();
+        System.out.println("in B");
+    }
+   
+    public B(int a) {
+        super();
+        System.out.println("in B int");
+    }
+
+}
 
 class Vipi {
 
     public static void main(String a[]) {
 
-        VeryAdvCalc obj = new VeryAdvCalc();
-       
-        int r1 = obj.add(4, 8);
-        int r2 = obj.sub(7, 3);
-        int r3 = obj.mul(4, 4);
-        int r4 = obj.div(9, 3);
-        double r5 = obj.power(5, 3);
-        System.out.println("r1:" + r1 + " " + "r2:" + r2 + " " + "r3:" + r3 + " " + "r4:" + r4+ " " + "r5:" + r5);
-        
-
-         }
+        B obj = new B(6);
+    }
 
 }
