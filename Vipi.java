41d94e7adf3f4a1595fc70fee811d7ac0f3eed98
Mark Mwima <markmwimar@gmail.com>
2024-08-05 13:35:51 +0300

    final variable constant

--- Vipi.java
+++ Vipi.java
@@ -1,35 +1,14 @@
 
-class A {
-  public void show() {
-    System.out.println("In A show");
-  }
-}
 
-
-class B extends A {
-  public void show() {
-    System.out.println("In B show");
-
-  }
-
-}
-
-class C extends A {
-  public void show() {
-    System.out.println("In c show");
-  }
-}
 
 
 class Vipi {
 
   public static void main(String a[]) {
 
-    A obj = new A();
-    obj = new B();
-    obj.show();
-
-    obj = new C();
-    obj.show();
+    final int num = 7;
+    
+    System.out.println(num);
+   
   }
 }
