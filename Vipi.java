d4fb3a9155e466476d40cef6c9d47eadcc6c56f5
Mark Mwima <markmwimar@gmail.com>
2024-08-08 08:16:54 +0300

    printing values

--- Vipi.java
+++ Vipi.java
@@ -8,8 +8,8 @@ class Vipi {
 
   public static void main(String a[]) {
    
-    Status r = Status.Running;
-    System.out.println(r);
+    Status[] rr = Status.values();
+    System.out.println(rr);
     
 
   }
