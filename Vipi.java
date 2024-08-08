94eb804859a123d4fe5a633ab4fc9b3ea822aa91
Mark Mwima <markmwimar@gmail.com>
2024-08-08 08:22:38 +0300

    enum if else

--- Vipi.java
+++ Vipi.java
@@ -9,9 +9,20 @@ class Vipi {
   public static void main(String a[]) {
    
     Status[] rr = Status.values();
+    Status s = Status.Running;
    
-    for (Status r : rr) {
-      System.out.println(r);
+    // for (Status r : rr) {
+    //   System.out.println(r);
+    // }
+
+    if (s == Status.Running) {
+      System.out.println("All good");
+    } else if (s == Status.Loading) {
+      System.out.println("Please wait");
+    } else if (s == Status.Success) {
+      System.out.println("Done");
+    } else {
+      System.out.println("Retry");
     }
 
   }
