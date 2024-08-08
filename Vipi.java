8f64c8b2c9e58d93e2f79a812c3449cd270f1caf
Mark Mwima <markmwimar@gmail.com>
2024-08-08 14:41:30 +0300

    method overriding

--- Vipi.java
+++ Vipi.java
@@ -1,20 +1,16 @@
-enum Laptop {
-  Thinkpad(400), HP(9000), Mac(3000), Acer(500);
-  private int price;
-
-  public int getPrice() {
-    return price;
+class A {
+  public void show() {
+    System.out.println("In A show");
   }
-
-  public void setPrice(int price) {
-    this.price = price;
-  }
-
-  private Laptop(int price) {
-    this.price = price;
-  }
-  
 }
+
+class B extends A {
+  public void show() {
+    System.out.println("In B show");
+  }
+}
+  
+
  
  
 
@@ -22,36 +18,5 @@ class Vipi {
 
   public static void main(String a[]) {
 
-    // Status[] rr = Status.values();
-    // Status s = Status.Success;
-   
-    // for (Status r : rr) {
-    //   System.out.println(r);
-    // }
-
-    // if (s == Status.Running) {
-    //   System.out.println("All good");
-    // } else if (s == Status.Loading) {
-    //   System.out.println("Please wait");
-    // } else if (s == Status.Success) {
-    //   System.out.println("Done");
-    // } else {
-    //   System.out.println("Retry");
-    // }
-//     switch (s) {
-//   case Running:
-//   System.out.println("All good");
-//   break;
-// case Loading:
-//  System.out.println("Please wait");
-//    break;
-//  case Success:
-//    System.out.println("Done");
-//    break;
-//  default:
-//    System.out.println("Timeout");
-//    break;
-
-// }
   }
 }
