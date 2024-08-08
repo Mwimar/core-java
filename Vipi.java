07a9b16890b05bb03b93ef0f8043ede8f80e30ee
Mark Mwima <markmwimar@gmail.com>
2024-08-08 08:30:14 +0300

    enum case switch

--- Vipi.java
+++ Vipi.java
@@ -7,23 +7,36 @@ enum Status {
 class Vipi {
 
   public static void main(String a[]) {
-   
+
     Status[] rr = Status.values();
-    Status s = Status.Running;
-   
+    Status s = Status.Success;
+
     // for (Status r : rr) {
     //   System.out.println(r);
     // }
 
-    if (s == Status.Running) {
-      System.out.println("All good");
-    } else if (s == Status.Loading) {
-      System.out.println("Please wait");
-    } else if (s == Status.Success) {
-      System.out.println("Done");
-    } else {
-      System.out.println("Retry");
-    }
+    // if (s == Status.Running) {
+    //   System.out.println("All good");
+    // } else if (s == Status.Loading) {
+    //   System.out.println("Please wait");
+    // } else if (s == Status.Success) {
+    //   System.out.println("Done");
+    // } else {
+    //   System.out.println("Retry");
+    // }
+    switch (s) {
+  case Running:
+  System.out.println("All good");
+  break;
+case Loading:
+ System.out.println("Please wait");
+   break;
+ case Success:
+   System.out.println("Done");
+   break;
+ default:
+   System.out.println("Timeout");
 
+}
   }
 }
