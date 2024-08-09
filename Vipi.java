516aac885b39165c2e537e13a865b8fe692feed3
Mark Mwima <markmwimar@gmail.com>
2024-08-09 10:46:54 +0300

    ascii values

--- Vipi.java
+++ Vipi.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 class A {
   public void show() throws ClassNotFoundException
   {
@@ -12,39 +14,42 @@ class Vipi {
     System.out.println("Class loaded");
   }
 
-  public static void main(String a[]) {
+  public static void main(String a[]) throws IOException {
     // A obj = (x, y) -> x + y;
-    
+
     // int result = obj.add(6, 3);
     // System.out.println(result);
-   
 
-  //   int x = 20;
-  //   int y = 0;
-    
-  //   try {
-  //     y = 18 / x;
-  //     if (y == 0) {
-  //       throw new MyException("Cannot divide by zero");//throwing exception to catch
-  //      }
-      
-  //   } catch (MyException e) {
-      
-  //     y = 18 / 1;
-  //     System.out.println("Default Output" + e);
-    
-  //   } catch (Exception e) {
-  //     System.out.println("Something went Wrong " + e);
-  //   }
-  //   System.out.println(y);
-  //   System.out.println("Bye");
+    //   int x = 20;
+    //   int y = 0;
 
-  A obj = new A();
-  try {
-    obj.show();
-  } catch (ClassNotFoundException e) {
-    // TODO Auto-generated catch block
-    e.printStackTrace();
-  }
+    //   try {
+    //     y = 18 / x;
+    //     if (y == 0) {
+    //       throw new MyException("Cannot divide by zero");//throwing exception to catch
+    //      }
+
+    //   } catch (MyException e) {
+
+    //     y = 18 / 1;
+    //     System.out.println("Default Output" + e);
+
+    //   } catch (Exception e) {
+    //     System.out.println("Something went Wrong " + e);
+    //   }
+    //   System.out.println(y);
+    //   System.out.println("Bye");
+
+    // A obj = new A();
+    // try {
+    //   obj.show();
+    // } catch (ClassNotFoundException e) {
+    //   // TODO Auto-generated catch block
+    //   e.printStackTrace();
+    // }
+
+    System.out.println("Enter a number");
+    int num = System.in.read();
+    System.out.println(num);
   }
 }
