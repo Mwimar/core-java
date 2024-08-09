0e066732b3a4e0082beb5b10fa75659da08b6cac
Mark Mwima <markmwimar@gmail.com>
2024-08-09 10:27:27 +0300

    exception checker function

--- Vipi.java
+++ Vipi.java
@@ -1,10 +1,14 @@
+class A {
+  public void show(){
 
-class MyException extends Exception {
-  MyException (String string) {
-    super(string);//passing to exception supreclass
+    try {
+       Class.forName("Vipi");
+         
+     } catch (ClassNotFoundException e) {
+       System.out.println("Class not found "+ e);
+     }
   }
 }
-
  
 
 class Vipi {
@@ -14,31 +18,26 @@ class Vipi {
     
     // int result = obj.add(6, 3);
     // System.out.println(result);
-    try {
-      Class.forName("Demo");
-        
-    } catch (ClassNotFoundException e) {
-      System.out.println("Class not found");
-    }
+   
 
-    int x = 20;
-    int y = 0;
+  //   int x = 20;
+  //   int y = 0;
     
-    try {
-      y = 18 / x;
-      if (y == 0) {
-        throw new MyException("Cannot divide by zero");//throwing exception to catch
-       }
+  //   try {
+  //     y = 18 / x;
+  //     if (y == 0) {
+  //       throw new MyException("Cannot divide by zero");//throwing exception to catch
+  //      }
       
-    } catch (MyException e) {
+  //   } catch (MyException e) {
       
-      y = 18 / 1;
-      System.out.println("Default Output" + e);
+  //     y = 18 / 1;
+  //     System.out.println("Default Output" + e);
     
-    } catch (Exception e) {
-      System.out.println("Something went Wrong " + e);
-    }
-    System.out.println(y);
-    System.out.println("Bye");
+  //   } catch (Exception e) {
+  //     System.out.println("Something went Wrong " + e);
+  //   }
+  //   System.out.println(y);
+  //   System.out.println("Bye");
   }
 }
