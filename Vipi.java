1b1ac8237035861e219cf2e6987cdbc50f80f468
Mark Mwima <markmwimar@gmail.com>
2024-08-10 08:09:57 +0300

    using hashmap

--- Vipi.java
+++ Vipi.java
@@ -1,8 +1,6 @@
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.TreeSet;
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 
@@ -11,27 +9,7 @@ class Vipi {
 
   public static void main(String a[]) throws InterruptedException {
 
-    Collection<Integer> nums = new TreeSet<Integer>();
-    nums.add(78);
-    nums.add(23);
-    nums.add(52);
-    nums.add(97);
-    // System.out.println(nums);
-
-    Iterator<Integer> values = nums.iterator();
-    while (values.hasNext()) {
-      
-      System.out.println(values.next());
-    }
-    
-
-    //     for (int n : nums) {
-    //   System.out.println(n);
-    // }
-//     for (Object n : nums) {
-//       int num = (Integer) n;
-//   System.out.println(n);
-// }
+    Map<String, Integer> students = new HashMap<>();
   }
 
 }
