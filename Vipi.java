7646d5b1f546d20f169023c669b60b180399dde8
Mark Mwima <markmwimar@gmail.com>
2024-08-10 22:18:39 +0300

    using function class

--- Vipi.java
+++ Vipi.java
@@ -1,6 +1,8 @@
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 
 
@@ -14,14 +16,27 @@ import java.util.List;
 class Vipi {
 
   public static void main(String a[]) throws InterruptedException {
+    
 
     List<Integer> nums = Arrays.asList(4, 5, 7, 3, 2, 6);
-    
+    Predicate<Integer> p = n -> n % 2 == 0;
+
+    Function<Integer, Integer> fun = new Function<Integer, Integer>() {
+
+      
+      public Integer apply(Integer n) {
+        
+        return n * 2;
+      }
+      
+    };
+                
+          
     // Stream<Integer> s1 = nums.stream();
     // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
     // Stream<Integer> s3 = s2.map(n -> n * 3);
 
-    int result = nums.stream().filter(n -> n % 2 == 0).map(n -> n * 2).reduce(0, (c,e) -> c+e);
+    int result = nums.stream().filter(p).map(fun).reduce(0, (c,e) -> c+e);
 
         System.out.println(result);
 
