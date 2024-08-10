f03ea3f65b7e0cc10fb850a913682dcb76e85a1d
Mark Mwima <markmwimar@gmail.com>
2024-08-10 22:29:26 +0300

    using stream sorted

--- Vipi.java
+++ Vipi.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 
 
@@ -21,24 +22,20 @@ class Vipi {
     List<Integer> nums = Arrays.asList(4, 5, 7, 3, 2, 6);
     Predicate<Integer> p = n -> n % 2 == 0;
 
-    Function<Integer, Integer> fun = new Function<Integer, Integer>() {
-
+    Function<Integer, Integer> fun = n ->  n * 2;
       
-      public Integer apply(Integer n) {
-        
-        return n * 2;
-      }
       
-    };
+    
                 
           
     // Stream<Integer> s1 = nums.stream();
     // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
     // Stream<Integer> s3 = s2.map(n -> n * 3);
 
-    int result = nums.stream().filter(p).map(fun).reduce(0, (c,e) -> c+e);
+    // int result = nums.stream().filter(p).map(fun).reduce(0, (c,e) -> c+e);
+    Stream <Integer> sortedValues = nums.stream().filter(p).sorted();
 
-        System.out.println(result);
+    sortedValues.forEach(n -> System.out.println(n));
 
     // s3.forEach(n -> System.out.println(n));
     
