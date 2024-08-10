da316fe3495f6e6eab698625f436dcc4e617c858
Mark Mwima <markmwimar@gmail.com>
2024-08-10 08:12:49 +0300

    inserting keys and values

--- Vipi.java
+++ Vipi.java
@@ -10,6 +10,13 @@ class Vipi {
   public static void main(String a[]) throws InterruptedException {
 
     Map<String, Integer> students = new HashMap<>();
+
+    students.put("Kev", 90);
+    students.put("Kodak", 80);
+    students.put("Durk", 70);
+    students.put("Future", 94);
+
+
   }
 
 }
