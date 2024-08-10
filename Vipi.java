854c9c5d611d0a546442cdb4611a14054e39bb99
Mark Mwima <markmwimar@gmail.com>
2024-08-10 19:50:21 +0300

    using Arrays class

--- Vipi.java
+++ Vipi.java
@@ -1,39 +1,13 @@
 
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 
 
 
-class Student implements Comparable<Student>{
 
-  int age;
-  String name;
-  
-  public String toString() {
-    return "Students [age=" + age + ", name=" + name + "]";
-  }
 
-  public Student(int age, String name) {
-    this.age = age;
-    this.name = name;
-  }
+  
 
-  @Override
-  public int compareTo(Student that) {
-    
-    if (this.age> that.age){
-        return 1;
-      }else {
-        return -1;
-      }
-    
-  }
-  
-  
-}
 
 
 
@@ -41,40 +15,8 @@ class Vipi {
 
   public static void main(String a[]) throws InterruptedException {
 
-    // Map<String, Integer> students = new Hashtable<>();
-
-    // students.put("Kev", 90);
-    // students.put("Kodak", 80);
-    // students.put("Durk", 70);
-    // students.put("Future", 94);
-    // students.put("Kev", 60);
-    // System.out.println(students.get("Kev"));
-    // // System.out.println(students.keySet());
-
-    // for (String key : students.keySet()){
-    //   System.out.println(key + ":"+ students.get(key));
-    // }
-    Comparator<Student> com = (Student i, Student j) ->   i.age> j.age? 1: -1;
+    List<Integer> nums =  Arrays.asList(5, 9, 1, 3, 8);
     
-     
-
-    
-
-
-
-    List<Student> studs = new ArrayList<>();
-    studs.add(new Student(25,"Mwima"));
-    studs.add(new Student(22,"Kodak"));
-    studs.add(new Student(24,"Jake"));
-    studs.add(new Student(35, "Palmer"));
-    
-    // Collections.sort(studs, com);
-    Collections.sort(studs);
-
-    for (Student s : studs) {
-      System.out.println(s);
-      
-    }
   
 
   }
