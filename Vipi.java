a000fed7e4299cb2849abc66ef8a92ba164ef2ce
Mark Mwima <markmwimar@gmail.com>
2024-08-10 18:04:51 +0300

    sorting using collectins

--- Vipi.java
+++ Vipi.java
@@ -1,6 +1,7 @@
 
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 
@@ -9,20 +10,27 @@ class Vipi {
 
   public static void main(String a[]) throws InterruptedException {
 
-    Map<String, Integer> students = new Hashtable<>();
+    // Map<String, Integer> students = new Hashtable<>();
 
-    students.put("Kev", 90);
-    students.put("Kodak", 80);
-    students.put("Durk", 70);
-    students.put("Future", 94);
-    students.put("Kev", 60);
-    System.out.println(students.get("Kev"));
-    // System.out.println(students.keySet());
+    // students.put("Kev", 90);
+    // students.put("Kodak", 80);
+    // students.put("Durk", 70);
+    // students.put("Future", 94);
+    // students.put("Kev", 60);
+    // System.out.println(students.get("Kev"));
+    // // System.out.println(students.keySet());
 
-    for (String key : students.keySet()){
-      System.out.println(key + ":"+ students.get(key));
-    }
+    // for (String key : students.keySet()){
+    //   System.out.println(key + ":"+ students.get(key));
+    // }
     
+    List<Integer> nums = new ArrayList<>();
+    nums.add(5);
+    nums.add(9);
+    nums.add(3);
+    nums.add(7);
+    Collections.sort(nums);
+System.out.println(nums);
 
 
   }
