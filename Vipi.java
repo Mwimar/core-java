8c4a58eaea8f079bfabdb48b69676161d3f634f1
Mark Mwima <markmwimar@gmail.com>
2024-08-10 20:06:39 +0300

    operations in arraylist

--- Vipi.java
+++ Vipi.java
@@ -15,9 +15,19 @@ class Vipi {
 
   public static void main(String a[]) throws InterruptedException {
 
-    List<Integer> nums =  Arrays.asList(5, 9, 1, 3, 8);
+    List<Integer> nums =  Arrays.asList(4, 5, 7, 3, 2,6);
+    int sum = 0;
+    for (int n : nums) {
+
+      if (n % 2 == 0) {
+        n = n * 2;
+        sum = sum + n;
+      }
+     
+    }
     
-  
+    System.out.println(sum);
+
 
   }
 
