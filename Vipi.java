68a4f795565e9e908b4633e91efd1f9abefe8790
Mark Mwima <markmwimar@gmail.com>
2024-08-10 08:20:34 +0300

    printing key set

--- Vipi.java
+++ Vipi.java
@@ -15,6 +15,10 @@ class Vipi {
     students.put("Kodak", 80);
     students.put("Durk", 70);
     students.put("Future", 94);
+    students.put("Kev", 60);
+    System.out.println(students.get("Kev"));
+    System.out.println(students.keySet());
+    
 
 
   }
