cc657c4b83f8cc31aee3dd7aee91492ad24b501c
Mark Mwima <markmwimar@gmail.com>
2024-08-10 07:01:42 +0300

    setting arraylist value types

--- Vipi.java
+++ Vipi.java
@@ -10,12 +10,17 @@ class Vipi {
 
   public static void main(String a[]) throws InterruptedException {
     
-    Collection nums = new ArrayList();
+    Collection<Integer> nums = new ArrayList<Integer>();
     nums.add(7);
     nums.add(2);
     nums.add(5);
+    nums.add(9);
 
+    System.out.println(nums);
 
+    for (int n : nums) {
+  System.out.println(n);
+}
   }
 
 }
