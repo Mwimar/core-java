9da4ef6e967d12e1e51cdb96dc16840fec3ea8c4
Mark Mwima <markmwimar@gmail.com>
2024-08-10 21:54:56 +0300

    stream reduce

--- Vipi.java
+++ Vipi.java
@@ -1,7 +1,6 @@
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 
 
@@ -18,10 +17,15 @@ class Vipi {
 
     List<Integer> nums = Arrays.asList(4, 5, 7, 3, 2, 6);
     
-    Stream<Integer> s1 = nums.stream();
-    Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
-    Stream<Integer> s3 = s2.map(n -> n * 3);
-    s3.forEach(n -> System.out.println(n));
+    // Stream<Integer> s1 = nums.stream();
+    // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
+    // Stream<Integer> s3 = s2.map(n -> n * 3);
+
+    int result = nums.stream().filter(n -> n % 2 == 0).map(n -> n * 2).reduce(0, (c,e) -> c+e);
+
+        System.out.println(result);
+
+    // s3.forEach(n -> System.out.println(n));
     
 
     
