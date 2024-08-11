4e7fec529d30915f0f7bcaf569b5e9c84dc74e8e
Mark Mwima <markmwimar@gmail.com>
2024-08-11 18:53:46 +0300

    optional class

--- StreamEx.java
+++ StreamEx.java
@@ -1,12 +1,16 @@
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 class StreamEx {
   public static void main(String[] args) {
     
-    List<String> names = Arrays.asList("Eenie", "Meenie", "Miney", "Moe");
+    List<String> names = Arrays.asList("Eenie", "Meenie", "Miney", "Mee");
+
+    Optional<String> name = names.stream().filter(str -> str.contains("o")).findFirst();
+    System.out.println(name.get());
    
   }
   
